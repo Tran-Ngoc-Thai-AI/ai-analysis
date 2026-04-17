b301dd0863b2e9e80ba0b011050bd02330dd9469
Ngoc Thai Tran <thaitran2801@gmail.com>
2026-04-17 23:56:47 +0700

    update hotfix Thêm xử lý an toàn cho pd.qcut trong visualization.py

--- utils/openai_client.py
+++ utils/openai_client.py
@@ -1,18 +1,23 @@
 from openai import AzureOpenAI
 import os
 
+input_api_key = ""
+input_azure_endpoint = ""
+input_model = ""
+
 client = AzureOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_version="2024-02-01"
+    api_key=input_api_key or os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=input_azure_endpoint or os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_version="2024-06-01"
 )
 
 def call_openai(prompt: str) -> str:
+    model = input_model or os.getenv("AZURE_OPENAI_DEPLOYMENT")
     response = client.chat.completions.create(
-        model=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+        model=model,
         messages=[
             {"role": "user", "content": prompt}
         ],
-        temperature=0.2
+        # temperature=0.2
     )
     return response.choices[0].message.content
